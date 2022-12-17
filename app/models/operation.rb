3f1c154df600b735a3f9e69d6f0a54453ad3e9f4
Atul Tiwari <atul.tiwari@coralmango.com>
2022-12-17 13:24:50 +0530

    added validation for feedback

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -4,6 +4,8 @@ class Operation < ApplicationRecord
   belongs_to :booth
   belongs_to :book
   has_many :categories, through: :booth
-  enum language: ["English", "Arabic"] 
+  enum language: ["English", "Arabic"]
+
+  validates :note, format: { with: /\A[a-zA-Z0-9.@#!|]+\z/, message: "Input Invalid Special Charecter not Allow!" }
 
 end
